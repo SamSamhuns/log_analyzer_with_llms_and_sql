b5c5df029f7331148ea7ea8ab3bc2a0aad3592e9
SamSamhuns <samhunsadamant@gmail.com>
2024-08-23 09:45:51 +0400

    Add app core cfg

---
+++ app/core/config.py
@@ -0,0 +1,30 @@
+"""
+Load configurations and constants 
+"""
+import os
+from logging.config import dictConfig
+from models.logging import LogConfig
+
+
+# project information
+PROJECT_NAME: str = "Log Summarizer API template"
+PROJECT_DESCRIPTION: str = "Template API for Log Summarizer"
+DEBUG: bool = os.environ.get("DEBUG", "") != "False"
+VERSION: str = "0.0.1"
+
+# save directories
+ROOT_STORAGE_DIR = os.getenv("ROOT_STORAGE_DIR", default="volumes/log_summarizer")
+FILE_STORAGE_DIR = os.getenv("FILE_STORAGE_DIR", default=os.path.join(ROOT_STORAGE_DIR, "files"))
+LOG_STORAGE_DIR = os.getenv("LOG_STORAGE_DIR", default=os.path.join(ROOT_STORAGE_DIR, "logs"))
+
+os.makedirs(ROOT_STORAGE_DIR, exist_ok=True)
+os.makedirs(FILE_STORAGE_DIR, exist_ok=True)
+os.makedirs(LOG_STORAGE_DIR, exist_ok=True)
+
+# logging conf
+log_cfg = LogConfig()
+# override info & error log paths
+log_cfg.handlers["info_rotating_file_handler"]["filename"] = os.path.join(LOG_STORAGE_DIR, "info.log")
+log_cfg.handlers["warning_file_handler"]["filename"] = os.path.join(LOG_STORAGE_DIR, "error.log")
+log_cfg.handlers["error_file_handler"]["filename"] = os.path.join(LOG_STORAGE_DIR, "error.log")
+dictConfig(log_cfg.model_dump())
