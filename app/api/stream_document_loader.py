a45ce9b8c6322cc68cd7e7b358ab4a048691cc13
SamSamhuns <samhunsadamant@gmail.com>
2024-08-23 16:54:32 +0400

    Add custom stream doc loader

---
+++ app/api/stream_document_loader.py
@@ -0,0 +1,44 @@
+from typing import Iterator, AsyncIterator
+from io import StringIO
+
+from langchain_core.document_loaders import BaseLoader
+from langchain_core.documents import Document
+
+
+class CustomStreamDocumentLoader(BaseLoader):
+    """A document loader that reads from a file stream line by line."""
+
+    def __init__(self, file_content: bytes, encoding: str = "utf-8") -> None:
+        """Initialize the loader with file content as bytes.
+
+        Args:
+            file_content: The content of the file as bytes.
+            encoding: encoding of the file content.
+        """
+        self.file_stream = StringIO(file_content.decode(encoding))
+
+    def lazy_load(self) -> Iterator[Document]:
+        """A lazy loader that reads from a file stream line by line synchronously."""
+        line_number = 0
+        line = self.file_stream.readline()
+        while line:
+            yield Document(
+                page_content=line,
+                metadata={"line_number": line_number, "source": "stream"},
+            )
+            line_number += 1
+            line = self.file_stream.readline()
+        self.file_stream.close()
+
+    async def alazy_load(self) -> AsyncIterator[Document]:
+        """An async lazy loader that reads from a file stream line by line asynchronously."""
+        line_number = 0
+        line = await self.file_stream.readline()
+        while line:
+            yield Document(
+                page_content=line,
+                metadata={"line_number": line_number, "source": "stream"},
+            )
+            line_number += 1
+            line = await self.file_stream.readline()
+        self.file_stream.close()
