405bc8b0739058df72ff7f734ac0db9533e95409
SamSamhuns <samhunsadamant@gmail.com>
2024-09-02 21:19:52 +0400

    Add test base server

---
+++ tests/routes/test_server.py
@@ -0,0 +1,16 @@
+"""
+Tests if server is running or not
+"""
+import pytest
+
+
+@pytest.mark.asyncio
+@pytest.mark.timeout(10)  # Timeout in seconds
+async def test_server_root(test_app_asyncio):
+    """
+    Test if fastapi+uvicorn server is up
+    """
+    response = await test_app_asyncio.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Welcome to the Log Analysis service": "Please visit /docs for list of apis"}
