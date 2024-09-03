a8377a6006d7ea578af7441c6f22c407c5366b3b
SamSamhuns <samhunsadamant@gmail.com>
2024-09-03 22:30:22 +0400

    Add type annots

--- tests/routes/test_server.py
+++ tests/routes/test_server.py
@@ -1,12 +1,13 @@
 """
 Tests if server is running or not
 """
+import httpx
 import pytest
 
 
 @pytest.mark.asyncio
 @pytest.mark.timeout(10)  # Timeout in seconds
-async def test_server_root(test_app_asyncio):
+async def test_server_root(test_app_asyncio: httpx.AsyncClient):
     """
     Test if fastapi+uvicorn server is up
     """
