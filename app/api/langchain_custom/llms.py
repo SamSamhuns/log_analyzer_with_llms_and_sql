d7cea89d8cbd7545c1dfa0e5dd3986ea634efd0a
SamSamhuns <samhunsadamant@gmail.com>
2024-09-03 14:36:23 +0400

    Select llm model with api

--- app/api/langchain_custom/llms.py
+++ app/api/langchain_custom/llms.py
@@ -1,18 +1,20 @@
 from langchain_openai import ChatOpenAI
 from langchain_community.llms.llamafile import Llamafile
-from core.config import LLM_MODEL_NAME
+from models.model import LLMModel
+
+
+def is_valid_model_value(model_value):
+    return any(model_value == item.value for item in LLMModel)
 
 
 def load_llm(llm_name: str):
-    if llm_name == "llamafile":
+    if llm_name == "Llamafile":
         # llama model server must be running for this
         # https://python.langchain.com/v0.1/docs/use_cases/question_answering/local_retrieval_qa/#llamafile
         # ./TinyLlama-1.1B-Chat-v1.0.Q5_K_M.llamafile --server --nobrowser
         return Llamafile()
-    elif llm_name == "chatopenai":
-        return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-1106")
+    elif is_valid_model_value(llm_name):
+        # defaults to openai models
+        return ChatOpenAI(temperature=0, model_name=llm_name)
     else:
         raise ValueError(f"Unsupported llm name: {llm_name}")
-
-# TODO update this file logic to be more robust and more llms
-llm = load_llm(LLM_MODEL_NAME)
