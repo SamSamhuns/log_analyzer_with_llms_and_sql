dd8fc99338b66dc699e2d12e9c14c9dd7957bc82
SamSamhuns <samhunsadamant@gmail.com>
2024-09-12 15:06:56 +0400

    Pass sql question in the body

--- app/routes/sql.py
+++ app/routes/sql.py
@@ -8,7 +8,7 @@ from fastapi import APIRouter, status, HTTPException
 
 from api.langchain_custom.text2sql import text_to_sql
 from api.mysql import run_sql_script, sep_query_and_params
-from models.model import SQLQueryParams, LogFileType, LLMModel
+from models.model import SQLQueryParams, QueryRequest, LogFileType, LLMModel
 from core.setup import mysql_conn, TEXT2SQL_CFG_DICT
 
 router = APIRouter()
@@ -24,12 +24,15 @@ async def sql_script(
     Runs an SQL query in the database with optional parameters.
     
     Args:
-    - query (str): The SQL query to execute.
-    - params (Optional[tuple]): Optional parameters for the SQL query.
+    - request_data (SQLQueryParams): The SQL query parameters, that contains query and params.
+            query (str): The SQL query to execute.
+            params (Optional[tuple]): Optional parameters for the SQL query.
 
-    Example params:
-        {"query": "UPDATE users SET name = %s, email = %s WHERE id = %s",
-        "params": ["Jane Doe", "jane.doe@example.com", 1]}
+    Example request body:
+        {
+            "query": "UPDATE users SET name = %s, email = %s WHERE id = %s",
+            "params": ["Jane Doe", "jane.doe@example.com", 1]
+        }
     """
     status_code = status.HTTP_200_OK
     response_data = {}
@@ -46,20 +49,27 @@ async def sql_script(
     return response_data
 
 
-@router.post("/qa/{LogFileType}", response_model=Dict,
+@router.post("/qa", response_model=Dict,
              status_code=status.HTTP_200_OK,
              summary="Convert query into sql command & interact with SQL database")
 async def sql_question_answer(
         log_type: LogFileType,
-        question: str,
+        question: QueryRequest,
         model: LLMModel = LLMModel.GPT_4o_Mini):
-    """Converts question into sql command & interact with SQL database"""
+    """
+    Converts query into sql command & interact with SQL database
+
+    Example request body:
+        {
+            "query": "What are the latest 5 records?"
+        }
+    """
     status_code = status.HTTP_200_OK
     response_data = {}
     try:
         text2sql_cfg_obj = TEXT2SQL_CFG_DICT[log_type.value]
         llm_sql_query = text_to_sql(
-            question=question,
+            question=question.query,
             text2sql_cfg_obj=text2sql_cfg_obj,
             llm_config={"model": model.value, "temperature": 0},
             top_k=text2sql_cfg_obj.top_k)
@@ -72,7 +82,7 @@ async def sql_question_answer(
                 mysql_conn, query, params, commit=not query.lower().startswith("select"))
             sql_data_response = {
                 "status": "success",
-                "question": question,
+                "question": question.query,
                 "query": llm_sql_query,
                 "response": sql_resp}
         except Exception as excep:
